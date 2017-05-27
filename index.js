5f6a841952c988eb75f081d1c599b57c4732deb8
jdlawrence <jamil.lawrence@gmail.com>
2017-05-26 18:10:36 -0700

    Dynamically creates a table

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ $(document).ready(() => {
   var boardArr = [];
   for (var i = 0; i < 3; i++) {
     // Add a row
-    boardArr.push([]); 
+    boardArr.push([]);
     for (var j = 0; j < 3; j++) {
       // Add a cell
       boardArr[i].push([0]);
     }
   }
-
-  $('tr').map(row => {
-    console.log('row', typeof row);
-  });
+  boardArr[1][1] = 234;
   
+  var tableBody = '';
+
+  $.each(boardArr, function(k, v) {
+    var tableRow = '';
+    // console.log('this', k, v);
+    $.each(this, function(k, v) {
+      // console.log('something', something);
+      tableRow += `<td>${v}</td>`;
+    });
+    tableBody += `<tr>${tableRow}</tr>`;
+  });
+  $('#app').append(tableBody);
 });
