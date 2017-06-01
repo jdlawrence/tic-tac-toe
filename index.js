5f83b1f8c7916dbfdd75d55e27fa3e17eceebf48
jdlawrence <jamil.lawrence@gmail.com>
2017-05-31 17:15:26 -0700

    Uses environment port

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 3005;
 
-app.use(express.static('public'));
-
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
 // });
 
-app.listen(3005, function () {
-  console.log('Example app listening on port 3005!');
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}`);
 });
