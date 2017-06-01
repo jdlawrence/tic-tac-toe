45e3138b9fd0ddd99281573fb6a5ff9014c3932e
jdlawrence <jamil.lawrence@gmail.com>
2017-05-31 21:49:48 -0700

    Removed commented out code

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3005;
 
-// app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}`);
